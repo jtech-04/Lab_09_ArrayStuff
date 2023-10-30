e21b118c7937eee2b6d3741e5dd60328d25648f4
jamie <jamiek0414@gmail.com>
2023-10-30 11:32:19 -0400

    final commit? pt 2

--- src/Main.java
+++ src/Main.java
@@ -91,6 +91,18 @@ public class Main
         return average;
     }
 
+    public static double getAverage(int dataPoints[])
+    {
+        double sum = 0.0;
+        double average = 0;
 
+        for (int l=0; l < dataPoints.length; l++)
+        {
+            sum += dataPoints[l];
+        }
+        average = sum/ dataPoints.length;
+
+        return average;
+    }
 
 }
